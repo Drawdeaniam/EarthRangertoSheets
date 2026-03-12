2126ee4c80382fc3c87d0f2e737bd67676f3f7d7
Drawdeaniam <125253941+Drawdeaniam@users.noreply.github.com>
2026-03-12 16:26:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,19 @@ from google.oauth2.service_account import Credentials
 ER_DOMAIN = "ack.pamdas.org"
 ER_TOKEN = os.getenv("ER_TOKEN")
 SHEET_ID = os.getenv("SHEET_ID")
-SERVICE_ACCOUNT_JSON = os.getenv("SERVICE_ACCOUNT_JSON")
+SERVICE_ACCOUNT_JSON = {
+  "type": "service_account",
+  "project_id": "earthranger-integration",
+  "private_key_id": "2eaa9d8a90ef0faac75614290e0322e861c48e6a",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC/BTxBBEnjDwHo\nrDUkACdSRetvZrPhmNyLhEurPD4bYpRQZYBcPv7fbk1TFAsQ10fbZPl5y99KUcxN\n7CDb/2gZq/utl0SSSuArOSsRweLa7TJ+UNK+/tsqDMqWunDaI+u59L0dB/7VvDrD\nADRn3hts+zsadsm7MN+flaMYvQgwE0kOM7ME+JdpF8s4eh/FZ9zs6qH6gG3np0WS\nrdwWTJLjpRcORXyI/wTK9+DGDz2jWwZ68wPwDOVeuPW2cM4S4KK0qyGhOTJ7xaGT\nUpDU4nMytV7tm0nyrVV2Gdjm2SXIz+LJ5oF9eHknABFBebKasEoI4AMMaSZPGIdS\npAHPG205AgMBAAECggEAC5Uf2gS5DYqyP8SlX5P0zmvNQqQpwcL46IAdQyT0J8lF\nRfoJm6OkOlxgAEAbCv/gFwuwht6XoP7ZvQXVg5F2jfacMaKWS//nR30AFrkOGN6W\nF7ZYjlqPY9AtE+ZP2SJMH6qZMaKeT0vKYlrnp+lTT2XiZ7s5MIpeR45qunbH1tD8\nM9Yv9c22yyPCyvpdR7gwQ0fIs9kQzxxq48BR8jwGGkrOgXU/WFOr6PSAt3buXmeH\nM4ZvorHmfFUWX58FPOTG/7twMqF2ipdI9eQ/1u2wbnbbop97ho/nxqOJK+UQIK24\paRlPavPowGaNO5mEUDV42h73SCVLwLsboiz73cRuwKBgQD5Tcikg4or0+w1da3a\nuDQ4Hjv8CSUJ7MclPh+2jj34m1tt84vte+crfzfk16Q5gX8Ha9it8WU+qFmzIzXf\n+cK/euw6skzV4daCrKZgfugbHicA8hFq3rpZRTZWyqlVmKpJ9hcE89MxLRopBkxC\npGoyvbZcglyEUmrRwQw2pgoWVwKBgQDEJrG9dAq7igLXwYrVSSXb1gMz0o6YcJoO\npX99Hp9g+wqjINxb6s8lDB4wetYjYyK5Z7+nBIlRjcKrp40eke1q2oCvrT77Wos4\nowQ/rZxUe/oQBeyer5MEs9ZeJVzJ5vPPee3bpBDYWHmMHfogMtCRA08P1vNTy3mj\nkM+roES+7wKBgDIeMALaqtFKxkatBKletKi/c0GkuPx4zEQxbACwMccjvEqrVmsE\nqKF27s6jh1FENjxQsvus9rSU9YtsvazsMfl6hbj7FTU+NHiKqtvlR1YERsUK5PN+\nGSpPHrBuB4K8sSczQMdvGPre3U54BKa1FOBkgR+x2VWEmBLY41KtKIP7AoGAD/n+\n3CmJfkD9fklbX4f3t3I1DJGH3868HZlJSw2leaa49RSGHk5/1Mqp4tT+gB9hAqz6\npUXvUV80jfq1udm09tEZTjXUPXDgihptDCq94vu+IHP7E+nFFcr4GO7+IcvX6/xI\nbW1tmdGLBOikKN86sbUNSYL+isK2A6aV46rILq0CgYBUIVI2uwE/ELiXO8O8m4uK\nSk2fgpfr13ocQqkubvsRErqoRBJyat2FtQ7aWbDjINFlCY23rzTQdtMTte2r2zz2\nyo5q7098bw+VzYU6LYaRnwitmVjDT4x1wNz0lOyEii8bsVZvi0eq7GdlEQOB7+T8\nj8UmkktM6PXbocwralB/8Q==\n-----END PRIVATE KEY-----\n",
+  "client_email": "er-sync-bot@earthranger-integration.iam.gserviceaccount.com",
+  "client_id": "105018899028643941740",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/er-sync-bot%40earthranger-integration.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
 
 SPECIES_REFERENCE = {
     "cheetah": "Carnivore", "lion": "Carnivore", "leopard": "Carnivore", 
@@ -142,4 +154,5 @@ if __name__ == "__main__":
         push_to_sheets({"Sync_Log": log_df})
     else:
         print("❌ No data received from EarthRanger.")
+
 
